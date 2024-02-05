7a93b6da297af36de4ca5adbe57be058eb08b82b
ntadevel <107974471+ntadevel@users.noreply.github.com>
2024-02-05 10:56:53 +0800

    add models of blog, post and viewed post. Add models to admin

--- blog_api/blog/admin.py
+++ blog_api/blog/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, Blog, ViewedPost
+
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ['user']
+
+
+admin.site.register(Blog, BlogAdmin)
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'text', 'create_time', 'blog']
+
+
+admin.site.register(Post, PostAdmin)
+
+
+class ViewedPostAdmin(admin.ModelAdmin):
+    list_display = ['user', 'post']
+
+
+admin.site.register(ViewedPost, ViewedPostAdmin)
--- blog_api/blog/models.py
+++ blog_api/blog/models.py
@@ -1,2 +1,33 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
+
+class Blog(models.Model):
+    """
+    Models of blog
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='blog', verbose_name='владелец')
+    follower = models.ManyToManyField(User, related_name='followers',
+                                      verbose_name='подписчики')
+
+
+class Post(models.Model):
+    """
+    Model of post
+    """
+    title = models.CharField(max_length=20, null=False, blank=False, verbose_name='заголовок')
+    text = models.CharField(max_length=140, verbose_name='текст')
+    create_time = models.DateTimeField(default=timezone.now(), verbose_name='дата создания')
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name='posts', verbose_name='блог')
+
+    def __str__(self):
+        return self.title
+
+
+class ViewedPost(models.Model):
+    """
+    Model of viewed post
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='users', verbose_name='пользователь')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='posts', verbose_name='пост')
